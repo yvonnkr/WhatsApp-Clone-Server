f2f0b6ced6e6cbcc31a74b373c11b28b2865e09c
yvonnkr <yvonnkr86@gmail.com>
2024-10-06 22:06:11 +0100

    Initial commit

---
+++ src/main/java/com/yvolabs/whatsappclone/WhatsappCloneBackApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.whatsappclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappCloneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yvolabs/whatsappclone/WhatsappCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
