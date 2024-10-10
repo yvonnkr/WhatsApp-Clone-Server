3cc219077b0ecb1f2b9f12be3b361ac0a2de4fa1
yvonnkr <yvonnkr86@gmail.com>
2024-10-10 16:50:20 +0100

    implement notification feature with server-sent-event

--- src/main/java/com/yvolabs/whatsappclone/infrastructure/secondary/message/SpringEventMessageChangeNotifier.java
+++ src/main/java/com/yvolabs/whatsappclone/infrastructure/secondary/message/SpringEventMessageChangeNotifier.java
@@ -5,6 +5,8 @@ import com.yvolabs.whatsappclone.messaging.domain.message.service.MessageChangeN
 import com.yvolabs.whatsappclone.messaging.domain.message.vo.ConversationPublicId;
 import com.yvolabs.whatsappclone.messaging.domain.user.vo.UserPublicId;
 import com.yvolabs.whatsappclone.shared.service.State;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +20,13 @@ import java.util.List;
 @Service
 public class SpringEventMessageChangeNotifier implements MessageChangeNotifier {
 
-    // TODO
+    private final NotificationService notificationService;
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    public SpringEventMessageChangeNotifier(ApplicationEventPublisher applicationEventPublisher, NotificationService notificationService) {
+        this.applicationEventPublisher = applicationEventPublisher;
+        this.notificationService = notificationService;
+    }
 
     @Override
     public State<Void, String> send(Message message, List<UserPublicId> userToNotify) {
@@ -26,8 +34,17 @@ public class SpringEventMessageChangeNotifier implements MessageChangeNotifier {
     }
 
     @Override
-    public State<Void, String> delete(ConversationPublicId conversationPublicId, List<UserPublicId> userToNotify) {
-        return null;
+    public State<Void, String> delete(ConversationPublicId conversationPublicId,
+                                      List<UserPublicId> userToNotify) {
+        ConversationIdWithUsers conversationIdWithUsers = new ConversationIdWithUsers(conversationPublicId, userToNotify);
+        applicationEventPublisher.publishEvent(conversationIdWithUsers);
+        return State.<Void, String>builder().forSuccess();
+    }
+
+    @EventListener
+    public void handleDeleteConversation(ConversationIdWithUsers conversationIdWithUsers) {
+        notificationService.sendMessage(conversationIdWithUsers.conversationPublicId().value(),
+                conversationIdWithUsers.users(), NotificationEventName.DELETE_CONVERSATION);
     }
 
 
