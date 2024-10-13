dbe256e05539091ee66b60d9dac98f80b4380dbe
yvonnkr <yvonnkr86@gmail.com>
2024-10-13 20:58:19 +0100

    implement get last seen endpoint

--- src/main/java/com/yvolabs/whatsappclone/infrastructure/primary/user/UsersResource.java
+++ src/main/java/com/yvolabs/whatsappclone/infrastructure/primary/user/UsersResource.java
@@ -2,7 +2,10 @@ package com.yvolabs.whatsappclone.infrastructure.primary.user;
 
 import com.yvolabs.whatsappclone.messaging.application.UsersApplicationService;
 import com.yvolabs.whatsappclone.messaging.domain.user.aggregate.User;
+import com.yvolabs.whatsappclone.messaging.domain.user.vo.UserPublicId;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * @author Yvonne N
@@ -43,5 +49,16 @@ public class UsersResource {
                 .stream().map(RestSearchUser::from)
                 .toList();
         return ResponseEntity.ok(searchResults);
+    }
+
+    @GetMapping("/get-last-seen")
+    ResponseEntity<Instant> getLastSeen(@RequestParam UUID publicId) {
+        Optional<Instant> lastSeen = usersApplicationService.getLastSeen(new UserPublicId(publicId));
+        if (lastSeen.isPresent()) {
+            return ResponseEntity.ok(lastSeen.get());
+        } else {
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Unable to fetch the presence of the user " + publicId);
+            return ResponseEntity.of(problemDetail).build();
+        }
     }
 }
